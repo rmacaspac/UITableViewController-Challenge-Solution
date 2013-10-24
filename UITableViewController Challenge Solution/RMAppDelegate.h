692a8d5b0c3a65697d9c674c267c06c4018a4b80
Ryan Macaspac <rmacaspac@gmail.com>
2013-10-23 21:37:15 -0700

    Initial Commit

---
+++ UITableViewController Challenge Solution/RMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RMAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Ryan Macaspac on 10/23/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/RMViewController.h
@@ -0,0 +1,13 @@
+//
+//  RMViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Ryan Macaspac on 10/23/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMViewController : UIViewController
+
+@end
